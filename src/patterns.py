3126acd3d5ec92a732f4c88055b45d32509b0ce5
Ramkumar Rajagopalan <cbe.ram@gmail.com>
2020-10-24 14:38:08 +0200

    Fix initial scaffolding

--- src/patterns.py
+++ src/patterns.py
@@ -4,7 +4,7 @@ class Patterns(object):
     def __init__(self):
         pass
 
-    def pattern1(n):
+    def pattern1(self, n):
         """
         Given an integer n, implement a function that prints a right triangle
         consisting of that many rows. For example, given an input of 4, print
@@ -15,7 +15,7 @@ class Patterns(object):
         """
         raise Exception("Not Implemented!")
 
-    def pattern2(n):
+    def pattern2(self, n):
         """
         Given an integer n, implement a function that prints an inverted
         triangle consisting of that many rows. For example, given an input
@@ -27,7 +27,7 @@ class Patterns(object):
         """
         raise Exception("Not Implemented!")
 
-    def pattern3(n):
+    def pattern3(self, n):
         """
         Combining the two patterns above, print two triangles. For example,
         given an input of 3, print
@@ -39,7 +39,7 @@ class Patterns(object):
         """
         raise Exception("Not Implemented!")
 
-    def pattern4(n):
+    def pattern4(self, n):
         """
         Similar to the above. Print a full diamond, with the longest row
         having '2*n-1' stars. for example, given an input of 3, print
@@ -60,4 +60,18 @@ if __name__ == "__main__":
         p = Patterns()
         num = sys.argv[1]
         fun = "pattern" + num
-        getattr(p, fun)()
+        try:
+            print(fun, 3)
+            getattr(p, fun)(3)
+        except Exception as e:
+            print(str(e))
+        try:
+            print(fun, 6)
+            getattr(p, fun)(6)
+        except Exception as e:
+            print(str(e))
+        try:
+            print(fun, 9)
+            getattr(p, fun)(9)
+        except Exception as e:
+            print(str(e))
